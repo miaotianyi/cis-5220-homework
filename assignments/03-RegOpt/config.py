0e577c72070e7f33f9bb56efa92debfc86e0dcc7
Tianyi Miao <tennielmiao@gmail.com>
2023-02-20 12:34:45 -0500

    change normalize std to 0.5

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -8,7 +8,7 @@ from torchvision.transforms import RandomHorizontalFlip
 
 class CONFIG:
     batch_size = 32
-    num_epochs = 10
+    num_epochs = 12
     initial_learning_rate = 0.01
     initial_weight_decay = 0.001
 
@@ -28,7 +28,20 @@ class CONFIG:
     transforms = Compose(
         [
             ToTensor(),
-            Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            # Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
             RandomHorizontalFlip(p=0.5),
         ]
     )
+
+
+# Gaussian noise(0, 0.1): 0.7/0.66
+# no noise: 0.7/0.66
+# rotate 30: 0.6/0.58
+# rotate 10: 0.66/0.63
+# horizontal flip p = 0.3: (~85 it/s) 0.69/0.64
+# normalize 0.5/0.5, horizontal flip: (~85 it/s) 0.72/0.68
+# normalize std=0.3, horizontal flip: (~85 it/s) 0.72/0.68
+# flip first, then to tensor: (~85 it/s): 0.72/0.69
+# normalize 0.5/0.5: 0.724/0.683
+# flip first: 0.721/0.679
